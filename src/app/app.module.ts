4d491defbf462345b7cc2f07c22da278f038c66b
draconical <draconiclove@gmail.com>
2024-10-04 14:11:50 +0400

    alternate loading init data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -38,8 +37,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,78 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { TreeNodeResponse } from "../models/common.model";
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  dataUrl = 'http://localhost:4200/assets/nodetree-mockup.json';
-
-  constructor(private http: HttpClient) { }
-
   loadData(): Observable<TreeNodeResponse> {
-    return this.http.get(this.dataUrl) as Observable<TreeNodeResponse>;
+    const data = {
+      "name": "guitars",
+      "children": [
+        {
+          "name": "acoustic",
+          "children": [
+            {
+              "name": "Kremona"
+            },
+            {
+              "name": "Epiphone"
+            },
+            {
+              "name": "Gibson"
+            },
+            {
+              "name": "Yamaha"
+            }
+          ]
+        },
+        {
+          "name": "electric",
+          "children": [
+            {
+              "name": "Fender",
+              "children": [
+                {
+                  "name": "Telecaster"
+                },
+                {
+                  "name": "Stratocaster"
+                },
+                {
+                  "name": "Jaguar"
+                }
+              ]
+            },
+            {
+              "name": "Gibson",
+              "children": [
+                {
+                  "name": "Les Paul"
+                },
+                {
+                  "name": "SG"
+                },
+                {
+                  "name": "ES-335"
+                },
+                {
+                  "name": "ES-339"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "acoustic bass"
+        },
+        {
+          "name": "electric bass"
+        }
+      ]
+    };
+
+    return of(data);
   }
 }
